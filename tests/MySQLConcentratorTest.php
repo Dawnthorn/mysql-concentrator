a38850d7d8eed907e69a5e9e85e0f1ab5c9ea3d1
Peter Haight <peterh@giantrabbit.com>
2012-08-20 15:05:38 -0700

    Moved some of the repeating code into methods. Made an interleaving test, but I looked at the packet log for that and it seems to send all the results for the query to the client after the initial request and doesn't wait for the client to fetch individual rows. So this doesn't really test what I'm trying to get at which is to force one query on one connection to interfere with a query on another connection.

--- tests/MySQLConcentratorTest.php
+++ tests/MySQLConcentratorTest.php
@@ -10,6 +10,7 @@ class MySQLConcentratorTest extends MySQLConcentratorBaseTest
   public $databases_config = null;
   public $db = null;
   public $dsn = null;
+  public $concentrator_dsn = null;
   public static $dsn_components  = array
   (
     'mysql' => array
@@ -35,6 +36,13 @@ class MySQLConcentratorTest extends MySQLConcentratorBaseTest
     return "$driver:" . implode(';', $components);
   }
 
+  function connect_to_concentrator()
+  {
+    $database_config = $this->databases_config['test'];
+    $connection = new MySQLConcentratorPDO($this->concentrator_dsn, $database_config['user_name'], $database_config['password']);
+    return $connection;
+  }
+
   function setUp()
   {
     parent::setUp();
@@ -49,30 +57,26 @@ class MySQLConcentratorTest extends MySQLConcentratorBaseTest
     $this->db->query("DROP TABLE IF EXISTS foo");
     $this->db->query("CREATE TABLE foo (id INTEGER AUTO_INCREMENT PRIMARY KEY, value VARCHAR(255))");
     $this->db->query("INSERT INTO foo (value) VALUES ('first')");
+    $database_config = $this->databases_config['test'];
+    $database_config['port'] = 3307;
+    $database_config['host'] = '127.0.0.1';
+    $this->concentrator_dsn = $this->build_dsn($database_config);
   }
 
   function testSimpleQueryOnSeparateConnections()
   {
-    $database_config = $this->databases_config['test'];
-    $database_config['port'] = 3307;
-    $database_config['host'] = '127.0.0.1';
-    $concentrator_dsn = $this->build_dsn($database_config);
-    $db_conn_1 = new MySQLConcentratorPDO($concentrator_dsn, $database_config['user_name'], $database_config['password']);
+    $db_conn_1 = $this->connect_to_concentrator();
     $result_1 = $db_conn_1->query("SELECT * FROM foo WHERE value = 'first'");
     $this->assertEqual(1, $result_1->rowCount());
-    $db_conn_2 = new MySQLConcentratorPDO($concentrator_dsn, $database_config['user_name'], $database_config['password']);
+    $db_conn_2 = $this->connect_to_concentrator();
     $result_2 = $db_conn_2->query("SELECT * FROM foo WHERE value = 'first'");
     $this->assertEqual(1, $result_2->rowCount());
   }
 
   function testTransactionOnOneConnectionWrapsOtherConnection()
   {
-    $database_config = $this->databases_config['test'];
-    $database_config['port'] = 3307;
-    $database_config['host'] = '127.0.0.1';
-    $concentrator_dsn = $this->build_dsn($database_config);
-    $db_conn_1 = new MySQLConcentratorPDO($concentrator_dsn, $database_config['user_name'], $database_config['password']);
-    $db_conn_2 = new MySQLConcentratorPDO($concentrator_dsn, $database_config['user_name'], $database_config['password']);
+    $db_conn_1 = $this->connect_to_concentrator();
+    $db_conn_2 = $this->connect_to_concentrator();
     $db_conn_1->query("BEGIN");
     $db_conn_2->query("INSERT INTO foo (value) VALUES ('second')");
     $result = $db_conn_2->query("SELECT * FROM foo WHERE value = 'second'");
@@ -81,4 +85,26 @@ class MySQLConcentratorTest extends MySQLConcentratorBaseTest
     $result = $db_conn_2->query("SELECT * FROM foo WHERE value = 'second'");
     $this->assertEqual(0, $result->rowCount());
   }
+
+  function testInterlevedQueriesOnOneConnection()
+  {
+    $db_conn_1 = $this->connect_to_concentrator();
+    $db_conn_1->query("INSERT INTO foo (value) VALUES ('second')");
+    $db_conn_1->query("INSERT INTO foo (value) VALUES ('third')");
+    $result_1 = $db_conn_1->query("SELECT * FROM foo ORDER BY id");
+    $this->assertEqual(3, $result_1->rowCount());
+    $row = $result_1->fetch();
+    $this->assertEqual('first', $row['value']);
+    $row = $result_1->fetch();
+    $this->assertEqual('second', $row['value']);
+    $db_conn_2 = $this->connect_to_concentrator();
+    $result_2 = $db_conn_2->query("SELECT * FROM foo ORDER BY id");
+    $row = $result_2->fetch();
+    $this->assertEqual('first', $row['value']);
+    $row = $result_1->fetch();
+    $this->assertEqual('third', $row['value']);
+    $row = $result_2->fetch();
+    $this->assertEqual('second', $row['value']);
+  }
+
 }
