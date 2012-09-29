31b03d6c045b9df779578b6624e0bd8943de80a6
Peter Haight <peterh@giantrabbit.com>
2012-09-29 15:29:32 -0700

    Updated to php-util 955d02cbca6.
    955d02cbca6f5b5cc4a80819bb6176605e04d609

--- contrib/php-util/string.php
+++ contrib/php-util/string.php
@@ -3,5 +3,5 @@
 function string_starts_with($haystack, $needle)
 {
   $length = strlen($needle);
-  return ((substr($haystack, 0, $length) === $needle);
+  return (substr($haystack, 0, $length) === $needle);
 }
